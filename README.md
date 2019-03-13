5c9a42d7ff63f44e4d6da9207e938c45fffb11ce
Pedro Freitas <pedrofreitas@outlook.com>
2019-03-12 21:46:03 -0300

    Primeiro exemplo de Thread

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # c-sharp
 Linguagem e seus Recursos C#
+
+
+# NETCORE CLI Commands
+
+dotnet new sln --name <SOLUTION_NAME> 
+Ex.: dotnet new sln --name C-Sharp
+
+dotnet new console --name <PROJECT_NAME> 
+Ex.: dotnet new console --name Thread
+
+dotnet sln <SOLUTION_NAME> add <PROJECT_NAME>
+Ex.: dotnet sln .\DesignPatterns.sln add .\Thread\Thread.csproj
+
+dotnet run --project <PROJECT_NAME> 
+Ex.: dotnet run --project .\Thread\Thread\Thread.csproj
---
+++ Threads/Thread_Basic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Basic {
+    class Program {
+        static void Main (string[] args) {
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.Start();
+
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("Main: " + i);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("Num: " + i);
+            }
+
+        }
+    }
+}

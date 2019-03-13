e3fa77e94e36df361415c1795c3d11e87b163a36
Pedro Freitas <pedrofreitas@outlook.com>
2019-03-12 22:09:59 -0300

    criação de mais exemplos básico de uso de Threads

---
+++ Threads/Thread_Background/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Background
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Data de Inicio: " + DateTime.Now);
+
+            for (int i = 0; i < 5; i++) {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.IsBackground = true;
+                t1.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+
+            for (int i = 0; i < 20; i++) {
+                Console.WriteLine("Num: " + i);
+            }
+
+            Console.WriteLine("DateTime: " + DateTime.Now);
+        }
+    }
+}

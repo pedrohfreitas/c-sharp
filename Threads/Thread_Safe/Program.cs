41665ab3db2a52fd70eea3b0813cb23e9c16eee8
Pedro Freitas <pedrofreitas@outlook.com>
2019-03-12 22:24:20 -0300

    Thread safe com o Lock

--- Threads/Thread_Safe/Program.cs
+++ Threads/Thread_Safe/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
 
 namespace Thread_Safe
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Rede = 0;
+        static object variavelDeControle = 0;
+
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Data de Inicio: " + DateTime.Now);
+
+            for (int i = 0; i < 5; i++) {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.IsBackground = true;
+                t1.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+
+            //O Lock trabalha com uma fila (Primeiro a entrar é o primeiro a sair)
+            lock(variavelDeControle){
+
+                for (int i = 0; i < 20; i++) {
+                    Console.WriteLine("Num: " + i);
+                    Rede++;
+                }
+            }
+
+            Console.WriteLine("DateTime: " + DateTime.Now);
         }
     }
 }

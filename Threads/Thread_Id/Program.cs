02380f606698166693d2ee01842027586d212230
Pedro Freitas <pedrofreitas@outlook.com>
2019-03-17 12:00:31 -0300

    criação de mais exemplos de uso de threads

---
+++ Threads/Thread_Id/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Id
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++) {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.Start(i);
+            }
+
+            Console.WriteLine("Programa finalizado");
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao(object Id) {
+
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("Thread:" + Id + " - Num: " + i + " - Managed Thread Id:" + Thread.CurrentThread.ManagedThreadId);
+            }
+
+        }
+    }
+}
---
+++ Threads/Thread_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sleep
+            Console.WriteLine("Inicio do programa - " + DateTime.Now);
+            Thread.Sleep(3000);
+            Console.WriteLine("Fim do programa - " + DateTime.Now);
+            
+            //Join
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.Start();
+            t1.Join();
+
+            Console.WriteLine("Fim do Join - " + DateTime.Now);
+
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+
+            for (int i = 0; i < 20; i++) {
+                Console.WriteLine("Num: " + i);
+            }
+
+            Console.WriteLine("DateTime: " + DateTime.Now);
+        }
+    }
+}
